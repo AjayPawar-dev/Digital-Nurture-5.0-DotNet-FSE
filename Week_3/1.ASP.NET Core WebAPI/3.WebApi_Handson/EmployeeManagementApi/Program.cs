af9df07b977a2b64121140cee99377d1002a0e6d
AjayPawar-dev <pawarajay2615@gmail.com>
2026-07-02 22:51:27 +0530

    Configured Swagger and global exception filter in ASP.NET Core pipeline

---
+++ Week_3/1.ASP.NET Core WebAPI/3.WebApi_Handson/EmployeeManagementApi/Program.cs
@@ -0,0 +1,25 @@
+using EmployeeManagementApi.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<CustomExceptionFilter>();
+});
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
