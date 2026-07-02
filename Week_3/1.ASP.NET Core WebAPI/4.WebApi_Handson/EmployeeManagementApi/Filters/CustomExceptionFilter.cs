e317c55883f848dcf23acdf88aa9c2994e267e65
AjayPawar-dev <pawarajay2615@gmail.com>
2026-07-02 23:29:41 +0530

    Added authorization and exception filters for Web API

---
+++ Week_3/1.ASP.NET Core WebAPI/4.WebApi_Handson/EmployeeManagementApi/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EmployeeManagementApi.Filters
+{
+    public class CustomExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            string path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "ErrorLog.txt");
+
+            File.AppendAllText(
+                path,
+                $"[{DateTime.Now}] {context.Exception}{Environment.NewLine}");
+
+            context.Result = new ObjectResult(new
+            {
+                Message = "Internal Server Error",
+                Error = context.Exception.Message
+            })
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
