3ecb9eaec1c6379f7edccd5dacf6376793f8bc72
AjayPawar-dev <pawarajay2615@gmail.com>
2026-07-05 23:01:10 +0530

    Implemented Kafka producer service using Confluent.Kafka

---
+++ Week_3/1.ASP.NET Core WebAPI/6.WebApi_Handson/EmployeeKafkaApi/Services/IKafkaProducerService.cs
@@ -0,0 +1,9 @@
+﻿using EmployeeKafkaApi.Models;
+
+namespace EmployeeKafkaApi.Services
+{
+    public interface IKafkaProducerService
+    {
+        Task ProduceAsync(Employee employee);
+    }
+}
---
+++ Week_3/1.ASP.NET Core WebAPI/6.WebApi_Handson/EmployeeKafkaApi/Services/KafkaProducerService.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using EmployeeKafkaApi.Models;
+using System.Text.Json;
+
+namespace EmployeeKafkaApi.Services
+{
+    public class KafkaProducerService : IKafkaProducerService
+    {
+        private readonly IProducer<Null, string> _producer;
+        private const string Topic = "employee-topic";
+
+        public KafkaProducerService()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        public async Task ProduceAsync(Employee employee)
+        {
+            string message = JsonSerializer.Serialize(employee);
+
+            await _producer.ProduceAsync(
+                Topic,
+                new Message<Null, string>
+                {
+                    Value = message
+                });
+
+            Console.WriteLine($"Message sent to Kafka: {message}");
+        }
+    }
+}
