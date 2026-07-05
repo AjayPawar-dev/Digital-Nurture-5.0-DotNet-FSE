3a47fbf413fc09a79fbd2a41f60f222637fc404b
AjayPawar-dev <pawarajay2615@gmail.com>
2026-07-05 23:01:09 +0530

    Added Employee model for Kafka producer

---
+++ Week_3/1.ASP.NET Core WebAPI/6.WebApi_Handson/EmployeeKafkaApi/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeKafkaApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public double Salary { get; set; }
+
+        public bool Permanent { get; set; }
+    }
+}
