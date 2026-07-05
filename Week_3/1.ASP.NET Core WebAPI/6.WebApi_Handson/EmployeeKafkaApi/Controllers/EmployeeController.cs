db7ee4319f18df78d59a8a97fc5339d34f69b26f
AjayPawar-dev <pawarajay2615@gmail.com>
2026-07-05 23:01:10 +0530

    Added Employee controller with Kafka message publishing endpoint

---
+++ Week_3/1.ASP.NET Core WebAPI/6.WebApi_Handson/EmployeeKafkaApi/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeKafkaApi.Models;
+using EmployeeKafkaApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeKafkaApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IKafkaProducerService _producerService;
+
+        public EmployeeController(IKafkaProducerService producerService)
+        {
+            _producerService = producerService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Employee employee)
+        {
+            await _producerService.ProduceAsync(employee);
+
+            return Ok("Employee sent to Kafka successfully.");
+        }
+    }
+}
